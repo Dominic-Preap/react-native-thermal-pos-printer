fb5082d20276b58787e0252502a931cb7fddc111
dominic-preap <preapchanoudom@gmail.com>
2026-04-28 15:01:09 +0700

    fix: update typo

--- src/classes/ReactNativePosPrinter.ts
+++ src/classes/ReactNativePosPrinter.ts
@@ -93,7 +93,7 @@ export class ReactNativePosPrinter {
         return this.currentDevice;
       }
 
-      const devices = await PosPrinter.getDeviceList(null);
+      const devices = await PosPrinter.getDeviceList({});
       const device = devices.find((d: ThermalPrinterNativeDevice) => d.address === address);
       
       if (!device) {
